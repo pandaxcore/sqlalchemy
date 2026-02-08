56f83090cc92c7a8002f968f2f2c8b80d06fb75d
pandaxcore <pandadesign7@gmail.com>
2026-02-09 00:03:23 +0500

    attempt to save data from xls III

--- read.py
+++ read.py
@@ -6,7 +6,8 @@ from sqlalchemy import String, Integer, Date
 from sqlalchemy.orm import mapped_column
 from typing import Optional
 from sqlalchemy.orm import Session
-from datetime import date, datetime
+from datetime import datetime
+from datetime import date as dt_date
 
 DB_USER = "postgres"
 DB_PASSWORD = "postgres"
@@ -38,7 +39,7 @@ class Person(Base):
     gender: Mapped[Optional[str]] = mapped_column(String(10))
     country: Mapped[Optional[str]] = mapped_column(String(50))
     age: Mapped[Optional[int]] = mapped_column(Integer)
-    date: Mapped[Optional[date]] = mapped_column(Date)
+    date: Mapped[Optional[dt_date]] = mapped_column(Date)
     tab_num: Mapped[int] = mapped_column(
         Integer, unique=True, nullable=False)
 
