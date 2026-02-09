931aa4f35cf6a120052bb04c32bb47775772f6eb
pandaxcore <pandadesign7@gmail.com>
2026-02-10 00:19:59 +0500

    test on xlsx and sqlalchemy

--- read.py
+++ read.py
@@ -1,66 +1,53 @@
-import pandas as pd
-from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
-from sqlalchemy import String, Integer, Date
 from sqlalchemy.orm import mapped_column
-from typing import Optional
+from sqlalchemy import String, Integer
+from datetime import date
+from sqlalchemy import create_engine
+import pandas as pd
 from sqlalchemy.orm import Session
-from datetime import datetime
-from datetime import date as dt_date
-
-DB_USER = "postgres"
-DB_PASSWORD = "postgres"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "postgres"
-URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-COLUMN_MAP = {
-    "First Name": "first_name",
-    "Last Name": "last_name",
-    "Gender": "gender",
-    "Country": "country",
-    "Age": "age",
-    "Date": "date",
-    "Id": "tab_num"
-}
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class Person(Base):
+class People(Base):
     __tablename__ = "people"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    gender: Mapped[Optional[str]] = mapped_column(String(10))
-    country: Mapped[Optional[str]] = mapped_column(String(50))
-    age: Mapped[Optional[int]] = mapped_column(Integer)
-    date: Mapped[Optional[dt_date]] = mapped_column(Date)
-    tab_num: Mapped[int] = mapped_column(
-        Integer, unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(30))
+    last_name: Mapped[str] = mapped_column(String(50))
+    gender: Mapped[str] = mapped_column(String(7))
+    country: Mapped[str] = mapped_column(String(50))
+    age: Mapped[int]
+    date: Mapped[date]
+    tab_num: Mapped[int] = mapped_column(Integer())
 
 
-engine = create_engine(URL)
+engine = create_engine(
+    "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres")
+
 Base.metadata.create_all(engine)
 
-df = pd.read_excel('file_example_XLS_10.xls')
+excel = pd.read_excel("file_example_XLS_10.xlsx")
 
-with Session(engine) as session:
-    for _, row in df.iterrows():
-        person = Person(
-            first_name=row["First Name"],
-            last_name=row["Last Name"],
-            gender=row["Gender"],
-            country=row["Country"],
-            age=int(row["Age"]),
-            date=datetime.strptime(row["Date"], "%d/%m/%Y").date(),
-            tab_num=int(row["Id"]),
-        )
-        print(person)
-        session.add(person)
-    session.commit()
-    session.close()
+excel["Date"] = pd.to_datetime(excel["Date"], dayfirst=True)
+excel["Country"] = excel["Country"].fillna("UNKNOWN")
+
+session = Session(engine)
+
+for index, row in excel.iterrows():
+    rows = People(
+        first_name=row["First Name"],
+        last_name=row["Last Name"],
+        gender=row["Gender"],
+        country=row["Country"],
+        age=row["Age"],
+        date=row["Date"],
+        tab_num=row["Id"]
+    )
+    session.add(rows)
+
+session.commit()
+session.close()
